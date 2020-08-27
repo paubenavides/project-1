cdfb474fa9504f6dfef343cce4d1934697762e99
paubenavides <70345050+paubenavides@users.noreply.github.com>
2020-08-27 14:29:58 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+basic.forever(function () {
+    basic.showNumber(0)
+    basic.showLeds(`
+        . . . . .
+        # # # . .
+        # # # # #
+        # . # # #
+        . . . . .
+        `)
+    basic.showIcon(IconNames.Heart)
+})
